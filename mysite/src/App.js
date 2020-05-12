98c98afff8f2e6531570462d4050e878e6250371
webbmaxwell <webbwrites@gmail.com>
2020-05-12 12:34:23 -0600

    Added title and started basic styling

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Link, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Body">
+        <h1 className="Title">Max Webb</h1>
+        <h3 className="Subtitle">Welcome to My Site</h3>
+      </div>
     </div>
   );
 }
